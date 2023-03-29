ff2e17c37423d6c3a52ae8c59bc6391e613caeb8
Ndimande <125868614+NdiHla@users.noreply.github.com>
2023-03-29 09:44:58 +0200

    IWA2

--- scripts.js
+++ scripts.js
@@ -36,7 +36,7 @@ if (newValue >= MAX_NUMBER) {
 subtract.addEventListener('click', subtractHandler)
 add.addEventListener('click', addHandler)
 
-/* This is the firstname of the user /*
+/* This is the firstname of the user */
 
 const user = 'John'
 
@@ -45,3 +45,25 @@ const user = 'John'
 
 
 console.log(user; surname;)
+
+/* (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014'
+
+console.log(date)
+
+/*
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn(Security scan starting)
+
+/* It is important to let user know when they can close the page  */
+
+cnosole.info('Please wait for scan to complete before closing the browser.)
